6e20ff6cea24f6519084e323eccd4bc29178ecd4
Avi Bhandari <86038001+hcgjhb@users.noreply.github.com>
2022-08-01 15:55:42 +0530

    Add files via upload

---
+++ HelloWorld.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.7.0;  //pragma tells the version in which it has to compile our solidity code
+
+contract Hello{                                         //declaring our Hello contract
+    function greeting() returns (string) {
+        return "Hello World";
+    }
+    
+}
